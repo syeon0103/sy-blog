6e09199755d3fa9053d8588ae2810d10ad4c1c50
suyeon <syeon0103@naver.com>
2024-10-09 13:55:24 +0900

    auth url 변경

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -1,7 +1,7 @@
 
 
 const clientId = 'b4d86e31fbdc4244a519de0df33e4488';
-let redirectUri = '';
+let redirectUri = 'https://sy-blog-vert.vercel.app/';
 const scopes = ['user-read-private', 'user-read-email', 'playlist-read-private'];
 
 
@@ -10,13 +10,13 @@ const scopes = ['user-read-private', 'user-read-email', 'playlist-read-private']
 
 const Auth = () => {
 
-    if (import.meta.env.VITE_REACT_APP_ENV === 'development') {
+ /*   if (import.meta.env.VITE_REACT_APP_ENV === 'development') {
         console.log("로컬 환경에서 실행 중입니다.")
         redirectUri = 'http://localhost:5173/'
     } else {
         console.log("배포 환경에서 실행 중입니다.")
         redirectUri =  'https://sy-blog-vert.vercel.app/'
-    }
+    }*/
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join('%20')}&response_type=token`;
     window.location.href = authUrl;
